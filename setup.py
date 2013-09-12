2fe47b8b5f70ca31ed1cffcabc1d27373363264a
Thomas Erlang <thomas@erlang.dk>
2013-09-12 15:05:09 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ mysql_requires = [
 
 setup(
     name='tlog',
-    version='0.1.9',
+    version='0.1.10',
     author='Thomas Erlang',
     author_email='thomas@erlang.dk',
     url='http://tesoft.dk/tlog',
