411c47b161352621c305215476bfe8a856448cef
Thomas Erlang <thomas@erlang.dk>
2013-09-12 15:07:43 +0200

    Version bump

--- src/tlog/migrate.py
+++ src/tlog/migrate.py
@@ -6,6 +6,9 @@ from tlog.config import Config
 from alembic import command
 
 def get_config():
+    logging.info(__file__)
+    logging.info(os.path.abspath(__file__))
+    logging.info(os.path.dirname(os.path.abspath(__file__)))
     logging.info('Trying to locate alembic.ini in {}'.format(os.path.dirname(os.path.abspath(__file__))+'/../../alembic.ini'))
     cfg = alembic.config.Config(os.path.dirname(os.path.abspath(__file__))+'/../../alembic.ini')
     cfg.set_main_option('script_location', 'tlog:migrations')
