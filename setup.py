7c8705292abca84e9d504c15522c3b2ac3eb6b9d
Thomas Erlang <thomas@erlang.dk>
2013-09-12 15:40:40 +0200

    Now it should work...

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ mysql_requires = [
 
 setup(
     name='tlog',
-    version='0.1.21',
+    version='0.1.22',
     author='Thomas Erlang',
     author_email='thomas@erlang.dk',
     url='http://tesoft.dk/tlog',
