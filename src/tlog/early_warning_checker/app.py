f93a0bc314a61317a41cd845215cf2bcdb0d8a2f
Thomas Erlang <thomas@erlang.dk>
2014-01-24 00:42:41 +0100

    Fixed stupid syntax error...

--- src/tlog/early_warning_checker/app.py
+++ src/tlog/early_warning_checker/app.py
@@ -10,10 +10,10 @@ def main():
         try:
             filters_to_check = Filter_warning.get_filters_to_check()
             Filter_warning.check_filter_warnings(filters_to_check)
-            
+
             Filter_inactivity.check()
         except Exception as e:
-            logging.exception('Exception in early warning checker'))
+            logging.exception('Exception in early warning checker')
         time.sleep(60)
 
 if __name__ == '__main__':
